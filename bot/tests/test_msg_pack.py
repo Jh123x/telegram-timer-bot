4ddb32cad85dae7dbbb9aed07522893849398b12
Wen Junhua <wenjh1998@gmail.com>
2024-05-09 22:28:39 +0800

    fix: fix tests

--- bot/tests/test_msg_pack.py
+++ bot/tests/test_msg_pack.py
@@ -1,6 +1,6 @@
 import unittest
-from msg_pack import MsgPack
-from constants import HELP
+from bot.msg_pack import MsgPack
+from bot.constants import HELP
 
 
 class TestMsgPack(unittest.TestCase):
