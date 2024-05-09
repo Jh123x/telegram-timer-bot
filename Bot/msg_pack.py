dc8965975541d7d623dda1e1d22609a3c1148c26
Wen Junhua <wenjh1998@gmail.com>
2024-05-09 21:30:54 +0800

    docs: update pydoc

--- Bot/msg_pack.py
+++ Bot/msg_pack.py
@@ -3,11 +3,14 @@ from pyrogram.types import InlineKeyboardMarkup
 
 class MsgPack:
     def __init__(self: 'MsgPack', text: str, markup: InlineKeyboardMarkup) -> None:
+        """MsgPack stores the message text and markup"""
         self.msg = text
         self.markup = markup
 
     def get_msg(self: 'MsgPack') -> str:
+        """Returns the message text"""
         return self.msg
 
     def get_markup(self: 'MsgPack') -> InlineKeyboardMarkup:
+        """Returns the message markup"""
         return self.markup
